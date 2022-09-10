92fe6ab08bcba1f69034beb13993c6c784481e80
RoseThorn10 <lrroman@comcast.net>
2022-09-10 10:19:34 -0500

    Added event handler function to script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,20 @@
+// format date and time in jumbotron
 var date = moment().format("LLLL");
-// { '#currentDay' }.text(date);
 $('#currentDay').text(date);
+
+// get input from textarea
+// save input to local storage as key(id), value(input) pair
+$(document).ready(function() {
+    $(".saveBtn").click(function(event) {   
+        console.log("Start Event");
+        var txt = $( event.target ).parent().children(".description").val();
+
+        var key = $( event.target ).parent().children(".description").attr("id");
+
+        console.log(`key ${key}, value ${txt}`);
+        localStorage.setItem(key, txt);
+
+        
+        
+    });
+});

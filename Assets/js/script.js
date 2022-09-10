f51480932a16922d4c65156bbe580e83d8ecc791
RoseThorn10 <lrroman@comcast.net>
2022-09-10 11:27:59 -0500

    Fixed event handler function, cosmetic changes to index.html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,9 +7,21 @@ $('#currentDay').text(date);
 $(document).ready(function() {
     $(".saveBtn").click(function(event) {   
         console.log("Start Event");
-        var txt = $( event.target ).parent().children(".description").val();
 
-        var key = $( event.target ).parent().children(".description").attr("id");
+        // Get the element that received the event
+        var targ = $( event.target );
+
+        // it might be the icon, so check, and if so
+        // get the parent (the button)
+        if ( $(targ).attr("class") == "fas fa-save") {
+            targ = $( targ ).parent();
+        }
+
+        // We have the button in targ, so get its sibling text box
+        // and return the id or text as needed
+        var txt = $( targ ).parent().children(".description").val();
+
+        var key = $( targ ).parent().children(".description").attr("id");
 
         console.log(`key ${key}, value ${txt}`);
         localStorage.setItem(key, txt);
